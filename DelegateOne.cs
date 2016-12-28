c81d566b0e2c6d6ecb88c8096946faf1c877361a
Vinay <vvk0403@gmail.com>
2016-12-28 15:29:54 -0600

    Add files via upload

---
+++ DelegateOne.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExDeligate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> c = new List<Customer>();
+            c.Add(new Customer() { id = 1, fname = "vinay", lname = "kumar" });
+            c.Add(new Customer() { id = 2, fname = "vin", lname = "ku" });
+            Customer c1 = new Customer()
+            {
+                id = 3,
+                fname = "vvv",
+                lname = "kkk"
+
+            };
+
+            c.Add(c1);
+            dprint d1 = new dprint(isprint);
+
+          
+            Customer.print(c,d1);
+
+
+            dvinayprint dv = new dvinayprint(Vinay.Vinayprint);
+            dv("vinay");
+            Console.ReadLine();
+            
+        }
+        public static bool isprint(Customer cust)
+        {
+            if (cust.id > 1)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    class Customer
+    {
+        public int id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public static void print(List<Customer> cus,dprint dp)
+        {
+
+            foreach(Customer member in cus)
+            {
+
+                if (dp(member))
+                {
+                    Console.WriteLine(member.id);
+                }
+            }
+        }
+    }
+    class Vinay
+    {
+        public static void Vinayprint(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+     delegate bool dprint(Customer customer);
+    public delegate void dvinayprint(string s);
+}
---
+++ DelegateTwo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Students> students = new List<Students>();
+            students.Add(new Students() {id=100,name="Vinay",score=65});
+            students.Add(new Students() { id = 101, name = "Kumar", score = 70 });
+            students.Add(new Students() { id = 102, name = "Raj", score = 80 });
+            students.Add(new Students() { id = 103, name = "IMCS", score = 35 });
+
+            delegateex d1 = new delegateex(Grade.grade);
+            Students.Grades(students, d1);
+            Console.ReadLine();
+        }
+    }
+
+    class Students
+    {
+        public int id { get; set; }
+        public int score { get; set; }
+        public string name { get; set; }
+        public static void Grades(List<Students> s,delegateex pass)
+        {
+            foreach(Students member in s)
+            {
+                if(pass(member))
+                {
+                    Console.WriteLine(member.id+" is Pass");
+
+                }
+                else
+                    Console.WriteLine(member.id + " is Fail");
+            }
+        }
+
+
+    }
+    class Grade
+    {
+        public static bool grade(Students st)
+        {
+            if (st.score > 35)
+                return true;
+            else
+                return false;
+        }
+    }
+    delegate bool delegateex(Students st);
+
+}
