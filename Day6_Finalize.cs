558b1f636a8e287c8413ed7916a1700e4deceb88
Vinay <vvk0403@gmail.com>
2016-12-29 21:05:14 -0600

    Add files via upload

---
+++ Day6_Finalize.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer(20,"Vinay");
+            Console.WriteLine(C1.ID +"  "+ C1.Name);
+
+            Customer C2 = new Customer();
+            C2.ID = 100;
+            C2.Name = "Kumar";
+
+            
+            Console.WriteLine(C2.ID +"  "+ C2.Name);
+            C1.Method();
+           // Console.ReadLine();
+        }
+        
+    }
+
+    class Customer
+    {
+        private int iD;
+        private string name;
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+    public    Customer(int ID, string Name)
+        {
+            iD = ID;
+            name = Name;
+            Console.WriteLine("with parameters");
+        }
+       public Customer()
+        {
+            Console.WriteLine("with out parameters");
+        }
+        public void Method()
+        {
+            Console.WriteLine("Vinay");
+        }
+
+        ~Customer()
+        {
+            Console.WriteLine("Finalizing Object");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Day6_IDisplosable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionsEx
+{
+    class Program :IDisposable
+    {
+      
+        static void Main(string[] args)
+        {
+            StreamReader s = null;
+            s= new StreamReader(@"C:\Drivers\vinay.txt");
+        
+            string fi = s.ReadToEnd();
+           
+            char[] charray = new char[10];
+          
+            int j = 0;
+            try
+            {
+                for (int i = 0; i < fi.Length; i++)
+                {
+
+                    char a = fi[i];
+                    charray[j] = a;
+                    j++;
+                }
+            }
+            catch (IndexOutOfRangeException ex1)
+            {
+                Console.WriteLine("Please check the size of the array");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                if (s != null)
+                    // ((IDisposable)s).Dispose();
+                    s.Dispose();
+                Console.ReadLine();
+
+            }
+            }
+            public void Dispose( )
+        {
+            Console.WriteLine("Dispose Method");
+
+             Console.ReadLine();
+        }
+
+       
+
+
+
+    }
+}
